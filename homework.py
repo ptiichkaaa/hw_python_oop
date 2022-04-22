ee3f09381f6845a38afbb126e62cf51624ec3aea
Владислав Соловейчик <soo.vlaad@gmail.com>
2022-04-22 19:10:38 +0300

    трекер

--- homework.py
+++ homework.py
@@ -6,13 +6,15 @@ class InfoMessage:
         self.distance = distance
         self.speed = speed
         self.calories = calories
-        
+
     def get_message(self) -> str:
-        return (f'Тип тренировки: {self.training_type}; '
+        return (
+            f'Тип тренировки: {self.training_type}; '
             f'Длительность: {self.duration:.3f} ч.; '
             f'Дистанция: {self.distance:.3f} км; '
             f'Ср. скорость: {self.speed:.3f} км/ч; '
-            f'Потрачено ккал: {self.calories:.3f}.')
+            f'Потрачено ккал: {self.calories:.3f}.'
+            )
 
 
 class Training:
